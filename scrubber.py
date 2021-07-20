b501d4e5cbebc62989e06ae7ccf43799c48ac17f
roscroft <adherrling@gmail.com>
2021-07-20 12:09:22 -0500

    Added input file cleaning notes

--- scrubber.py
+++ scrubber.py
@@ -1,9 +1,13 @@
 import re
+# Cleaning input file:
+# Replace Alt-010 (line break) with space
+# Remove all commas
+
 scrubbed = []
 desired_columns = [0,10,11,12,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]
 # All rank abbreviations, surrounded by spaces
 sub_dict = {}
-sub_dict["rank abbrev"] = ("PV1|PVT|PFC|SPC|CPL|SGT|SSG|SFC|MSG|1SG|SGM|CSM|Chief|Mr.|Mrs.|Ms.|WO1|WO2|WO3|WO4|WO5|W01|W02|W03|W04|W05|CW1|CW2|CW3|CW4|CW5|1LT|2LT|LT|CPT|MAJ|LTC|COL|BG|MG|LTG|GEN", "SM")
+sub_dict["rank abbrev"] = ("PV1|PVT|PFC|SPC|CPL|SGT|SSG|SFC|MSG|1SG|SGM|CSM|Chief|Mr\.|Mrs\.|Ms\.|WO1|WO2|WO3|WO4|WO5|W01|W02|W03|W04|W05|CW1|CW2|CW3|CW4|CW5|1LT|2LT|LT|CPT|MAJ|LTC|COL|BG|MG|LTG|GEN", "SM")
 sub_dict["rank"] = ("private( first class)?|specialist|corporal|(staff |master |first )?sergeant( first class| major)?|(first |second |1st |2nd )?lieutenant|captain|major|(lieutenant )?colonel", "SM")
 #sub_dict["comp"] = ("NCO|NCOIC|OIC", " SM ")
 sub_dict["possessive pronoun"] = ("his|hers", "their")
