409d0de709bf011e60660e92847a7a9f3a294c04
roscroft <adherrling@gmail.com>
2021-07-20 12:02:27 -0500

    Moved nicknames to separate file

--- scrubber.py
+++ scrubber.py
@@ -6,8 +6,6 @@ sub_dict = {}
 sub_dict["rank abbrev"] = ("PV1|PVT|PFC|SPC|CPL|SGT|SSG|SFC|MSG|1SG|SGM|CSM|Chief|Mr.|Mrs.|Ms.|WO1|WO2|WO3|WO4|WO5|W01|W02|W03|W04|W05|CW1|CW2|CW3|CW4|CW5|1LT|2LT|LT|CPT|MAJ|LTC|COL|BG|MG|LTG|GEN", "SM")
 sub_dict["rank"] = ("private( first class)?|specialist|corporal|(staff |master |first )?sergeant( first class| major)?|(first |second |1st |2nd )?lieutenant|captain|major|(lieutenant )?colonel", "SM")
 #sub_dict["comp"] = ("NCO|NCOIC|OIC", " SM ")
-# Manually found nicknames
-sub_dict["nicknames"] = ("Nick|Greg|Ozzy|Viera|Vic|Jon|Brandon|Alex|Louie|Rich|Bill|DJ|Cuba|Russ|Ben|Milt|Josh|Myers|Chris", "SM")
 sub_dict["possessive pronoun"] = ("his|hers", "their")
 sub_dict["nominative pronoun"] = ("him|her", "them")
 sub_dict["they"] = ("he|she", "they")
@@ -16,6 +14,10 @@ sub_dict["was"] = ("they was", "they were")
 sub_dict["has"] = ("they has", "they have")
 sub_dict["'s"] = ("they's", "they're")
 sub_dict["’s"] = ("they’s", "they're")
+
+with open ("./nicknames.txt", "r+") as nickname_file:
+    nicknames = "|".join([line.rstrip() for line in nickname_file])
+    sub_dict["nicknames"] = nicknames
 
 def regex_generator(match):
     # Regex breakdown:
